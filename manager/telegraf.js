33ffcb10d0759d8d8ec9a468c6cc3edd6709f2b2
whmountains <8680147+whmountains@users.noreply.github.com>
2017-12-20 16:41:49 -0300

    Throttle telegraf writes

--- manager/telegraf.js
+++ manager/telegraf.js
@@ -1,14 +1,15 @@
 const dgram = require('dgram')
+const throttle = require('lodash.throttle')
 
 const telegrafHost = 'localhost'
 const telegrafPort = 8094
 
 const socket = dgram.createSocket('udp4')
 
-const telegrafWrite = reading => {
+const telegrafWrite = throttle(reading => {
   const message = JSON.stringify(reading)
 
   socket.send(message, telegrafPort, telegrafHost)
-}
+}, 5000)
 
 module.exports = { telegrafWrite }
