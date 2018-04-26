92e3b4ea86870e254e2f29c839011f97b6518c2b
whmountains <8680147+whmountains@users.noreply.github.com>
2018-04-26 10:34:43 -0300

    fix

--- manager/inputs.js
+++ manager/inputs.js
@@ -21,7 +21,7 @@ const inputs = [
 exports.readInputs = async () => {
   const result = {}
 
-  inputs.forEach(({ name, pin }) => {
+  inputs.forEach(({ name, gpioInstance }) => {
     result[name] = !gpioInstance.readSync() ? 1 : 0
     console.log(name, result[name])
     // console.log(name, 'direct', !new require('onoff').Gpio(19, 'in').readSync() ? 1 : 0)
