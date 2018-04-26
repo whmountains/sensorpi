b061857a3471c718d869049965db5dc0b0085aea
whmountains <8680147+whmountains@users.noreply.github.com>
2018-04-26 09:36:54 -0300

    Update board map

--- manager/gpio.js
+++ manager/gpio.js
@@ -1,17 +1,12 @@
 const R = require('ramda')
-
-// conditionally mock the Gpio module
 let { Gpio } = require('onoff')
 
 // map of ports to output
 const portMap = {
-  // 1: 23,
-  2: 24,
-  3: 27,
-  // 4: 22,
-  5: 18,
-  6: 4,
-  // 7: 11,
+  1: 17,
+  2: 27,
+  3: 22,
+  4: 10,
 }
 
 // this holds the port state
@@ -37,7 +32,7 @@ const writePort = (port, value) => {
   outputRegister[port] = value
 }
 
-const writePorts = portValueMap => {
+const writePorts = (portValueMap) => {
   R.forEachObjIndexed((value, port) => {
     writePort(port, value)
   }, portValueMap)
