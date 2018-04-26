f9a7e304f68fe43b61b6994ef3acd1cb70bcdf86
whmountains <8680147+whmountains@users.noreply.github.com>
2018-04-26 10:39:09 -0300

    remove console log

--- manager/inputs.js
+++ manager/inputs.js
@@ -23,9 +23,6 @@ exports.readInputs = async () => {
 
   inputs.forEach(({ name, gpioInstance }) => {
     result[name] = !gpioInstance.readSync() ? 1 : 0
-    console.log(name, result[name])
-    // console.log(name, 'direct', !new require('onoff').Gpio(19, 'in').readSync() ? 1 : 0)
-    // result[name] = !new require('onoff').Gpio(19, 'in').readSync() ? 1 : 0
   })
 
   return result
